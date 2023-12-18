7cc96a48ae0116e777aee1b85fad1b7b351de3db
JemmaPhillips <jemma_phillips1@hotmail.co.uk>
2023-12-18 14:18:43 +0000

    Add Python work

---
+++ Python/basicspythonact1.py
@@ -0,0 +1,17 @@
+# a
+result_a = -5 + 8 * 6
+
+# b
+result_b = (55 + 9) % 9
+
+# c
+result_c = 20 + -3 * 5 / 8
+
+# d
+result_d = 5 + 15 / 3 * 2 - 8 % 3
+
+# Print the results
+print("Result of -5 + 8 * 6 is:", result_a)
+print("Result of (55 + 9) % 9 is:", result_b)
+print("Result of 20 + -3 * 5 / 8 is:", result_c)
+print("Result of 5 + 15 / 3 * 2 - 8 % 3 is:", result_d)
