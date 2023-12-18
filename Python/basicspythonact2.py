1ca43300299ef196ff6e3809bbe603af48598a93
JemmaPhillips <jemma_phillips1@hotmail.co.uk>
2023-12-18 14:49:29 +0000

    add python activity 2

---
+++ Python/basicspythonact2.py
@@ -0,0 +1,9 @@
+#Get numbers
+first_num = float(input("Input First Number: "))
+second_num = float(input("Input Second Number: "))
+
+#divide
+result = first_num / second_num
+
+#print result
+print(result)
